e6e621afc1587aeb66cb5b51d6b35b8e15aa43a2
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-08-07 16:19:36 +0900

    ストロングパラメータの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,11 @@ class ItemsController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :charges_id, :source_id, :days_id, :price).merge(user_id: current_user.id)
+  end
+  
 end
