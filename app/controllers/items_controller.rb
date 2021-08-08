a2dcf772a265146efd01dd0596c85c83a917b50c
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-08-08 10:02:06 +0900

    出品商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,22 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :description
-    validates :category_id
+    validates :price, numericality: { in: 300..9999999 }
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :choice1
+  belongs_to :choice2
+  belongs_to :choice3
+  belongs_to :choice4
+  belongs_to :choice5
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id 
     validates :condition_id
     validates :charges_id
     validates :source_id
     validates :days_id
-    validates :price
   end
+
 end
