4bccc7b6c46cf82fbc79243b68d7d37cc900265f
Heidiki <82322691+Heidiki@users.noreply.github.com>
2021-09-01 23:11:45 +0900

    dbの整理

--- db/migrate/20210811124722_rename_charges_id_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameChargesIdColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :charges_id, :charge_id
-  end
-end
--- db/migrate/20210811130003_rename_scheduled_delivery_column_to_items.rb
+++
@@ -1,5 +0,0 @@
-class RenameScheduledDeliveryColumnToItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :items, :scheduled_delivery, :scheduled_delivery_id
-  end
-end
